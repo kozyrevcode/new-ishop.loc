f1feddcd823ac9e42cd9c25d802f9546b4bde9ca
Kozyrev Ilya <kozyrev.dev@gmail.com>
2022-04-11 09:41:53 +0300

    создаёт классы App и Registry

--- public/index.php
+++ public/index.php
@@ -1,7 +1,15 @@
 <?php
 
+use wfm\App;
+
 if (PHP_MAJOR_VERSION < 8) {
     die('Необходима версия PHP >= 8');
 }
 
 require_once dirname(__DIR__) . '/config/init.php';
+
+new App();
+
+// echo App::$app->getProperty('pagination');
+// App::$app->setProperty('test', 'test');
+// var_dump(App::$app->getPropetries());
